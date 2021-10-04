a7d912b8cdf30afcbf431f5cf022ca5da68f856f
janjanchen <ssp.18s20.janicechen@gmail.com>
2021-10-05 00:32:31 +0800

    Add Event Class

--- src/main/java/seedu/address/model/event/Event.java
+++ src/main/java/seedu/address/model/event/Event.java
@@ -1,4 +1,142 @@
 package seedu.address.model.event;
 
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+import seedu.address.model.tag.Tag;
+
+/**
+ * Represents an Event in SoConnect.
+ * Guarantees: details are present and not null, field values are validated, immutable.
+ */
 public class Event {
+    //Compulsory fields
+    private final Name name;
+    private final DateAndTime startDateAndTime;
+
+    //Optional fields
+    private final DateAndTime endDateAndTime;
+    private final Description description;
+    private final Address address;
+    private final Zoom zoom;
+
+    private final Set<Tag> tags = new HashSet<>();
+
+    /**
+     * All fields must be present and not null (currently).
+     */
+    public Event(Name name, DateAndTime startDateAndTime, DateAndTime endDateAndTime,
+                 Description description, Address address, Zoom zoom, Set<Tag> tags) {
+        requireAllNonNull(name, startDateAndTime, endDateAndTime, description, address, zoom, tags);
+        this.name = name;
+        this.startDateAndTime = startDateAndTime;
+        this.endDateAndTime = endDateAndTime;
+        this.description = description;
+        this.address = address;
+        this.zoom = zoom;
+        this.tags.addAll(tags);
+    }
+
+    public Name getName() {
+        return name;
+    }
+
+    public DateAndTime getStartDateAndTime() {
+        return startDateAndTime;
+    }
+
+    public DateAndTime getEndDateAndTime() {
+        return endDateAndTime;
+    }
+
+    public Description getDescription() {
+        return description;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public Zoom getZoom() {
+        return zoom;
+    }
+
+    /**
+     * Returns an immutable tag set, which throws {@code UnsupportedOperationException}
+     * if modification is attempted.
+     */
+    public Set<Tag> getTags() {
+        return Collections.unmodifiableSet(tags);
+    }
+
+    /**
+     * Returns true if both events have the same name.
+     * This defines a weaker notion of equality between two events.
+     */
+    public boolean isSameEvent(Event otherEvent) {
+        if (otherEvent == this) {
+            return true;
+        }
+
+        return otherEvent != null
+                && otherEvent.getName().equals(getName());
+    }
+
+    /**
+     * Returns true if both events have the same details.
+     * This defines a stronger notion of equality between two events.
+     */
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof Event)) {
+            return false;
+        }
+
+        Event otherEvent = (Event) other;
+        return otherEvent.getName().equals(getName())
+                && otherEvent.getStartDateAndTime().equals(getStartDateAndTime())
+                && otherEvent.getEndDateAndTime().equals(getEndDateAndTime())
+                && otherEvent.getDescription().equals(getDescription())
+                && otherEvent.getAddress().equals(getAddress())
+                && otherEvent.getZoom().equals(getZoom())
+                && otherEvent.getTags().equals(getTags());
+    }
+
+    @Override
+    public int hashCode() {
+        // use this method for custom fields hashing instead of implementing your own
+        return Objects.hash(name, startDateAndTime, endDateAndTime, description,
+                address, zoom, tags);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append(getName())
+                .append("; Start: ")
+                .append(getStartDateAndTime())
+                .append("; End: ")
+                .append(getEndDateAndTime())
+                .append("; Description: ")
+                .append(getDescription())
+                .append("; Address: ")
+                .append(getAddress())
+                .append("; Zoom: ")
+                .append(getZoom());
+
+        Set<Tag> tags = getTags();
+        if (!tags.isEmpty()) {
+            builder.append("; Tags: ");
+            tags.forEach(builder::append);
+        }
+        return builder.toString();
+    }
 }
