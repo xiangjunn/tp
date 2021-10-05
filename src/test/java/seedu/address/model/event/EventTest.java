699100687565d5e9c15c7b7c4c51d9e229da473c
janjanchen <ssp.18s20.janicechen@gmail.com>
2021-10-05 23:01:00 +0800

    Add Event Test Classes

---
+++ src/test/java/seedu/address/model/event/EventTest.java
@@ -0,0 +1,52 @@
+package seedu.address.model.event;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EventTest {
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getStartDateAndTime() {
+    }
+
+    @Test
+    void getEndDateAndTime() {
+    }
+
+    @Test
+    void getDescription() {
+    }
+
+    @Test
+    void getAddress() {
+    }
+
+    @Test
+    void getZoom() {
+    }
+
+    @Test
+    void getTags() {
+    }
+
+    @Test
+    void isSameEvent() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
---
+++ src/test/java/seedu/address/model/event/UniqueEventListTest.java
@@ -0,0 +1,40 @@
+package seedu.address.model.event;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UniqueEventListTest {
+
+    @Test
+    void contains() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void setEvent() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void setEvents() {
+    }
+
+    @Test
+    void testSetEvents() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+}
