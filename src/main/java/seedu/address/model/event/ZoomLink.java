ac3bc4fb77fe6131b3d69cc6f59e993019c71422
Gordon25 <73068242+Gordon25@users.noreply.github.com>
2021-10-08 03:05:46 +0800

    Resolve merge conflict

--- src/main/java/seedu/address/model/event/ZoomLink.java
+++ src/main/java/seedu/address/model/event/ZoomLink.java
@@ -9,35 +9,35 @@ import static java.util.Objects.requireNonNull;
  */
 public class ZoomLink {
 
-public static final String MESSAGE_CONSTRAINTS =
+    public static final String MESSAGE_CONSTRAINTS =
             "Zoom link should be in http://[link].com format";
 
-// TODO: 10/6/2021 add validation
-public static final String VALIDATION_REGEX = ".*";
+    // TODO: 10/6/2021 add validation
+    public static final String VALIDATION_REGEX = ".*";
 
-public final String value;
+    public final String value;
 
-/**
-* Constructs a {@code ZoomLink}.
-*
-* @param link A valid zoom link.
-*/
-public ZoomLink(String link) {
-    requireNonNull(link);
-    value = link;
-}
+    /**
+    * Constructs a {@code ZoomLink}.
+    *
+    * @param link A valid zoom link.
+    */
+    public ZoomLink(String link) {
+        requireNonNull(link);
+        value = link;
+    }
 
-// TODO: 10/6/2021 add test cases for invalid zoom link
-/**
- * Returns true if a given string is a valid zoom link.
- */
-public static boolean isValidZoomLink(String test) {
-    return test.matches(VALIDATION_REGEX);
-}
+    // TODO: 10/6/2021 add test cases for invalid zoom link
+    /**
+     * Returns true if a given string is a valid zoom link.
+     */
+    public static boolean isValidZoomLink(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
 
-@Override
-public boolean equals(Object other) {
-    return other == this // short circuit if same object
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
                 || (other instanceof ZoomLink // instanceof handles nulls
                 && value.equals(((ZoomLink) other).value)); // state check
     }
