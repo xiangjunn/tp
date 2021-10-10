9ebf6b3d88b919ef972632f59eebce57f56d3bcc
Ng Xiang Jun <ngxiangjun1999@gmail.com>
2021-10-11 01:04:18 +0800

    Update cclear command

--- src/main/java/seedu/address/logic/commands/contact/CClearCommand.java
+++ src/main/java/seedu/address/logic/commands/contact/CClearCommand.java
@@ -4,22 +4,21 @@ import static java.util.Objects.requireNonNull;
 
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.CommandResult;
-import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 
 /**
- * Clears the address book.
+ * Clears the contacts.
  */
 public class CClearCommand extends Command {
 
-    public static final String COMMAND_WORD = "clear";
-    public static final String MESSAGE_SUCCESS = "Address book has been cleared!";
+    public static final String COMMAND_WORD = "cclear";
+    public static final String MESSAGE_SUCCESS = "Contacts have been cleared!";
 
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
-        model.setAddressBook(new AddressBook());
+        model.resetContacts();
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
