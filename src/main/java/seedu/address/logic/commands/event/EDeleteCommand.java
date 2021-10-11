86f1316291a07e52934d9dad2e19216eb50a2c51
janjanchen <ssp.18s20.janicechen@gmail.com>
2021-10-11 13:27:34 +0800

    Add EDeleteCommand and EDeleteCommandParser

---
+++ src/main/java/seedu/address/logic/commands/event/EDeleteCommand.java
@@ -0,0 +1,56 @@
+package seedu.address.logic.commands.event;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.List;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.CommandResult;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.event.Event;
+
+/**
+ * Deletes an event identified using it's displayed index from the SoConnect.
+ */
+public class EDeleteCommand extends Command {
+
+    public static final String COMMAND_WORD = "edelete";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Deletes the event identified by the index number used in the displayed SoConnect.\n"
+            + "Parameters: INDEX1[-INDEX2] (both indexes must be a positive integer)\n"
+            + "Example: " + COMMAND_WORD + " 1";
+
+    public static final String MESSAGE_DELETE_EVENT_SUCCESS = "Deleted Event: %1$s";
+
+    private final Index targetIndex;
+
+    public EDeleteCommand(Index targetIndex) {
+        this.targetIndex = targetIndex;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        List<Event> lastShownList = model.getFilteredEventList();
+
+        if (targetIndex.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_EVENT_DISPLAYED_INDEX);
+        }
+
+        Event eventToDelete = lastShownList.get(targetIndex.getZeroBased());
+        model.deleteEvent(eventToDelete);
+        return new CommandResult(String.format(MESSAGE_DELETE_EVENT_SUCCESS, eventToDelete));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof EDeleteCommand // instanceof handles nulls
+                && targetIndex.equals(((EDeleteCommand) other).targetIndex)); // state check
+    }
+}
+
