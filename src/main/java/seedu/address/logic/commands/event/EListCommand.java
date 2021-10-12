5d02afefcfbe579a2ac80be67f85f1335d41a0b1
Gordon25 <73068242+Gordon25@users.noreply.github.com>
2021-10-12 23:48:00 +0800

    Edit EListCommand to allow optional fields

--- src/main/java/seedu/address/logic/commands/event/EListCommand.java
+++ src/main/java/seedu/address/logic/commands/event/EListCommand.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.commands.event;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_HIDE_ALL_EVENTS;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_EVENTS;
 
 import seedu.address.logic.commands.Command;
@@ -20,7 +21,9 @@ public class EListCommand extends Command {
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
+        model.updateFilteredEventList(PREDICATE_HIDE_ALL_EVENTS);
         model.updateFilteredEventList(PREDICATE_SHOW_ALL_EVENTS);
         return new CommandResult(MESSAGE_SUCCESS);
     }
+
 }
