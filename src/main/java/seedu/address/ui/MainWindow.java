4d0fd8f778b55a586fa3778905cbe044784a31e2
chunweii <47494777+chunweii@users.noreply.github.com>
2021-10-20 00:55:37 +0800

    Calendar UI: Add menu item dropdown and shortcut

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -44,6 +44,9 @@ public class MainWindow extends UiPart<Stage> {
     private MenuItem helpMenuItem;
 
     @FXML
+    private MenuItem calendarItem;
+
+    @FXML
     private StackPane contactListPanelPlaceholder;
 
     @FXML
@@ -79,6 +82,7 @@ public class MainWindow extends UiPart<Stage> {
 
     private void setAccelerators() {
         setAccelerator(helpMenuItem, KeyCombination.valueOf("F1"));
+        setAccelerator(calendarItem, KeyCombination.valueOf("F2"));
     }
 
     /**
