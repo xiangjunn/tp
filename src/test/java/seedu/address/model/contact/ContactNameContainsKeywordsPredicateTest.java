f6fa38dc0d7ce304e19d26ea8eb405a3a99234ae
Gordon25 <73068242+Gordon25@users.noreply.github.com>
2021-10-25 20:00:04 +0800

    add more test cases to increase code cov

--- src/test/java/seedu/address/model/contact/ContactNameContainsKeywordsPredicateTest.java
+++ src/test/java/seedu/address/model/contact/ContactNameContainsKeywordsPredicateTest.java
@@ -84,9 +84,10 @@ public class ContactNameContainsKeywordsPredicateTest {
         assertFalse(predicate.test(new PersonBuilder().withName("Alice Bob").build()));
 
         // Keywords match phone, email and address, but does not match name
-        predicate = new ContactNameContainsKeywordsPredicate(Arrays.asList("12345",
-                "alice@email.com", "Main", "Street"));
+        predicate = new ContactNameContainsKeywordsPredicate(Arrays.asList("22345"));
+        predicate.setEmailKeywords(Arrays.asList("alex@email.com"));
+        predicate.setTagKeywords(Arrays.asList("smart", "cca"));
         assertFalse(predicate.test(new PersonBuilder().withName("Alice").withPhone("12345")
-                .withEmail("alice@email.com").withAddress("Main Street").build()));
+                .withEmail("alice@email.com").withAddress("Main Street").withTags("friends").build()));
     }
 }
