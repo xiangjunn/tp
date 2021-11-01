4a3932e11a3383fd8ae371fd3a5d68cb2091eaa6
janjanchen <ssp.18s20.janicechen@gmail.com>
2021-11-01 16:46:55 +0800

    Add EViewCommandParserTest

--- src/test/java/seedu/address/logic/parser/event/EViewCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/event/EViewCommandParserTest.java
@@ -1,4 +1,4 @@
-package seedu.address.logic.parser.contact;
+package seedu.address.logic.parser.event;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
@@ -7,43 +7,42 @@ import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.logic.commands.contact.CViewCommand;
+import seedu.address.logic.commands.event.EViewCommand;
 
-public class CViewCommandParserTest {
-
-    private CViewCommandParser parser = new CViewCommandParser();
+public class EViewCommandParserTest {
+    private EViewCommandParser parser = new EViewCommandParser();
 
     @Test
     public void parse_emptyArg_throwsParseException() {
         assertParseFailure(parser, "     ",
-                String.format(MESSAGE_INVALID_COMMAND_FORMAT, CViewCommand.MESSAGE_USAGE));
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, EViewCommand.MESSAGE_USAGE));
     }
 
     @Test
     public void parse_validArgs_returnsViewCommand() {
         // no leading and trailing whitespaces and only one valid index
-        CViewCommand expectedCViewCommand =
-                new CViewCommand(INDEX_FIRST);
-        assertParseSuccess(parser, "1", expectedCViewCommand);
+        EViewCommand expectedEViewCommand =
+                new EViewCommand(INDEX_FIRST);
+        assertParseSuccess(parser, "1", expectedEViewCommand);
     }
 
     @Test
     public void parse_invalidArgs_throwsParseException() {
         // multiple indexes
         assertParseFailure(parser, "1 2 3",
-                String.format(MESSAGE_INVALID_COMMAND_FORMAT, CViewCommand.MESSAGE_USAGE));
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, EViewCommand.MESSAGE_USAGE));
 
         //no index specified
         assertParseFailure(parser, "",
-                String.format(MESSAGE_INVALID_COMMAND_FORMAT, CViewCommand.MESSAGE_USAGE));
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, EViewCommand.MESSAGE_USAGE));
 
         //zero index
         assertParseFailure(parser, "0",
-                String.format(MESSAGE_INVALID_COMMAND_FORMAT, CViewCommand.MESSAGE_USAGE));
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, EViewCommand.MESSAGE_USAGE));
 
         //negative index
         assertParseFailure(parser, "-10",
-                String.format(MESSAGE_INVALID_COMMAND_FORMAT, CViewCommand.MESSAGE_USAGE));
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, EViewCommand.MESSAGE_USAGE));
 
     }
 }
